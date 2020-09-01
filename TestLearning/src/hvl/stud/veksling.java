634d38918e43d4df1c0a7efa1c1200e36f9f83d5
Elkjøp Låne-PC <Elkjp Lne-PC@158.37.234.236>
2020-09-01 15:42:33 +0200

    its is what it is

---
+++ TestLearning/src/hvl/stud/veksling.java
@@ -0,0 +1,18 @@
+package hvl.stud;
+
+public class veksling {
+	public static void main(String[] args) {
+		int pris=100;
+		int betalt=99;
+		
+		if(betalt < pris) {
+			System.out.println("Kunden betalt for lite!");
+		}else {
+			int pengerTilbake=betalt-pris;
+			int tikroner=pengerTilbake/10;
+			int kroner=pengerTilbake%10;
+			System.out.println("Tilbake: "+tikroner+ " tikroner"+
+			" og "+kroner+ " kronestykker");
+		}
+	}
+}
