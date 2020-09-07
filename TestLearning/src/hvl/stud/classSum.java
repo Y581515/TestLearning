b04e8200c8ce59a80750b05d2bd9a4da6c38f669
Elkjøp Låne-PC <Elkjp Lne-PC@DESKTOP-0QLCH2F>
2020-09-07 22:07:24 +0200

    classSum og return

---
+++ TestLearning/src/hvl/stud/classSum.java
@@ -0,0 +1,18 @@
+package hvl.stud;
+
+public class classSum {
+
+	public static void main(String[] args) {
+		
+		double sum =sumRekke(2,0.5,5);
+		System.out.println(sum);
+		
+		}
+	private static double sumRekke(int a, double k,int n) {
+		double sum=0;
+		for (int i=0;i<=n-1;i++) {
+			sum=sum+a*Math.pow(k, i);
+		}
+		return sum;
+	}
+}
---
+++ TestLearning/src/hvl/stud/learningReturn.java
@@ -0,0 +1,18 @@
+package hvl.stud;
+
+public class learningReturn {
+
+	public static void main(String[] args) {
+		int minste=minst(69,420);
+		System.out.println(minste);}
+		
+	public static int minst(int tall1,int tall2) {
+			if(tall1 < tall2) {
+				return tall1;
+			}
+			return tall2;  
+		}
+
+	}
+
+
