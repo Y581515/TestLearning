f4ff6982c672cf8749f878437bbc46aeed0519e2
Elkjøp Låne-PC <Elkjp Lne-PC@158.37.234.243>
2020-09-08 18:12:55 +0200

    j

---
+++ TestLearning/src/hvl/stud/prøvetest.java
@@ -0,0 +1,17 @@
+package hvl.stud;
+
+public class prøvetest {
+
+	public static void main(String[] args) {
+int tall = 7540;
+        
+        do {
+            tall = tall / 10;
+        } while (tall != 0);
+        
+        System.out.println(tall);
+		
+
+	}
+
+}
