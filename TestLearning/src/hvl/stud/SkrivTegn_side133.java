6184ca4cb8b872811c2599949991aa49dc993f5f
yfess <yfess@DESKTOP-MO3P69F>
2020-09-29 04:48:12 +0200

    skrivtegn

--- TestLearning/src/hvl/stud/SkrivTegn_side133.java
+++ TestLearning/src/hvl/stud/SkrivTegn_side133.java
@@ -4,16 +4,17 @@ public class SkrivTegn_side133 {
 
 	public static void main(String[] args) {
 		
-		skrivTegn(e,3);
+		skrivTegn("pussy",3);
+	}
+
 		
-		private static void skrivTegn(char t, int antall) {
+		private static void skrivTegn(String s, int antall) {
 			
 			for (int i = 1; i <= antall; i++) {
-				System.out.print(t);
+				System.out.print(s+" ");
 			}
 			System.out.println();
 		}
 
-	}
 
 }
